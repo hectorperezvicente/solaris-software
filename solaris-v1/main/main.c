1c92deb9271fec1a129763514d12acf25ab2ff6e
Oscar Barja <oscar.barja@alumnado.uvigo.gal>
2026-02-18 14:28:18 +0000

    BMP driver adapted to new transmit function and init stack size changed; devcontainer fixed to flash submodule ref updated

--- solaris-v1/main/main.c
+++ solaris-v1/main/main.c
@@ -20,11 +20,11 @@ static const char* TAG = "MAIN";
 static bmp_data_t s_bmp;
 static icm_data_t s_icm;
 
-// static void bmp_init_task(void *arg)
-// {
-//     BmpInit(arg);
-//     SPP_OSAL_TaskDelete(NULL);
-// }
+static void bmp_init_task(void *arg)
+{
+    BmpInit(arg);
+    SPP_OSAL_TaskDelete(NULL);
+}
 
 void app_main(void)
 {
@@ -77,13 +77,13 @@ void app_main(void)
         }
     }
 
-    // Step 8: Configure Interrupt Pin Settings
-    // s_bmp.int_pin       = (spp_uint32_t)INT_GPIO;
-    // s_bmp.int_intr_type = (spp_uint32_t)GPIO_INTR_POSEDGE;
-    // s_bmp.int_pull      = 0;
+    //Step 8: Configure Interrupt Pin Settings
+    s_bmp.int_pin       = (spp_uint32_t)INT_GPIO;
+    s_bmp.int_intr_type = (spp_uint32_t)GPIO_INTR_POSEDGE;
+    s_bmp.int_pull      = 0;
 
-    // Step 9: Create BMP Initialization Task
-    // xTaskCreate(bmp_init_task, "bmp_init", BMP_INIT_TASK_STACK_SIZE, &s_bmp, BMP_INIT_PRIO, NULL);
+    //Step 9: Create BMP Initialization Task
+    xTaskCreate(bmp_init_task, "bmp_init", BMP_INIT_TASK_STACK_SIZE, &s_bmp, BMP_INIT_PRIO, NULL);
 
     
     SPP_LOGI("APP", "Application starting...");
@@ -97,27 +97,27 @@ void app_main(void)
     // Core_Init();
 
     // // Getting one SPP packet
-    // spp_packet_t *p_packet_1 = SPP_DATABANK_getPacket();
-    // p_packet_1->primary_header.version = 0xFA;
-    // ret = SPP_DATABANK_returnPacket(p_packet_1);
+    spp_packet_t *p_packet_1 = SPP_DATABANK_getPacket();
+    p_packet_1->primary_header.version = 0xFA;
+    ret = SPP_DATABANK_returnPacket(p_packet_1);
 
-    // // Following the logic this will have to return the same address of packet as p_packet_1
-    // spp_packet_t *p_packet_2 = SPP_DATABANK_getPacket();
-    // // We can check the new data is being written
-    // p_packet_2->primary_header.version = 0xFE;
-    // ret = SPP_DATABANK_returnPacket(p_packet_2);
+    // Following the logic this will have to return the same address of packet as p_packet_1
+    spp_packet_t *p_packet_2 = SPP_DATABANK_getPacket();
+    // We can check the new data is being written
+    p_packet_2->primary_header.version = 0xFE;
+    ret = SPP_DATABANK_returnPacket(p_packet_2);
 
     
 
-    // vTaskDelay(pdMS_TO_TICKS(100));
+    vTaskDelay(pdMS_TO_TICKS(100));
 
-    // // Step 10: Read Altitude Measurement
-    // float altitude = 0.0f;
-    // ret = bmp390_get_altitude(p_spi_bmp, &s_bmp, &altitude);
-    // if (ret != SPP_OK) {
-    //     while (1) {}
-    // }
+    // Step 10: Read Altitude Measurement
+    float altitude = 0.0f;
+    ret = bmp390_get_altitude(p_spi_bmp, &s_bmp, &altitude);
+    if (ret != SPP_OK) {
+        SPP_LOGE(TAG, "Failed to read altitude from BMP390");
+    }
 
-    // vTaskDelay(pdMS_TO_TICKS(50));
+    vTaskDelay(pdMS_TO_TICKS(50));
 
 }
