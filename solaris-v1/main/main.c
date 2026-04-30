3c37b1c8a7683b464f6beba039b81c755316b3b6
Silent Whisper <132281492+secureshadow@users.noreply.github.com>
2026-04-30 21:08:08 +0200

    Enable SPP_DEBUG_PRINT by default; remove debug subscriber from main

--- solaris-v1/main/main.c
+++ solaris-v1/main/main.c
@@ -2,8 +2,6 @@
 #include "spp/services/bmp390/bmp390.h"
 #include "spp/services/icm20948/icm20948.h"
 #include "spp/services/datalogger/datalogger.h"
-#include "spp/services/pubsub/pubsub.h"
-#include <stdio.h>
 
 extern const SPP_HalPort_t g_esp32HalPort;
 
@@ -35,22 +33,6 @@ static Datalogger_t s_logger = {
     .p_filePath = "/sdcard/log.txt",
 };
 
-static uint32_t s_rxCount = 0U;
-
-static void debugSubscriber(const SPP_Packet_t *p_packet, void *p_ctx)
-{
-    (void)p_ctx;
-    s_rxCount++;
-    if (s_rxCount % 50U == 0U)
-    {
-        printf("[DEBUG] rx=%u apid=0x%04X queue=%u overflow=%u\n",
-               (unsigned)s_rxCount,
-               (unsigned)p_packet->primaryHeader.apid,
-               (unsigned)SPP_SERVICES_PUBSUB_queueDepth(),
-               (unsigned)SPP_SERVICES_PUBSUB_overflowCount(K_SPP_APID_ALL));
-    }
-}
-
 void app_main(void)
 {
     (void)SPP_CORE_boot(&g_esp32HalPort);
@@ -62,9 +44,6 @@ void app_main(void)
     (void)SPP_SERVICES_register(&g_icm20948Module, &s_icm);
     (void)SPP_SERVICES_register(&g_bmp390Module, &s_bmp);
     (void)SPP_SERVICES_register(&g_sdLoggerModule, &s_logger);
-
-    SPP_SERVICES_PUBSUB_subscribe(K_SPP_APID_ALL, K_SPP_PUBSUB_PRIO_LOW,
-                                   debugSubscriber, NULL);
 
     for (;;)
     {
